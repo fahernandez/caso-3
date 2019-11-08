d40ea98fc322c662775fb44e40f56e3f1bf72073
anibalb05 <anibalbrenesj@gmail.com>
2019-11-08 15:41:25 -0600

    change factors to numeric

--- main.R
+++ main.R
@@ -8,8 +8,30 @@ library(RColorBrewer)
 
 
 # Read data
+#setwd("C:/Users/abrenes/Documents/GitHub/caso-3")
 data<- read.spss("./data/caso3.sav", to.data.frame=TRUE, use.value.labels = TRUE)
-attr(data, "variable.labels")
+attr(data, "variable.labels")[44:51]
 summary(data)
 names(data)
-view(data)
+#view(data)
+table(data$Preferencia)
+table(data$ACC1)
+head(as.numeric(data$ACC1),17)
+
+
+
+#data[,2:9] = apply(data[,2:9], MARGIN = 2, FUN = as.numeric)
+## Access data
+levels_ACC = levels(data$ACC1)
+for (i in 2:9){
+  data[,i] = as.numeric(data[,i])
+}
+
+
+## COnfidence data
+levels_CON = levels(data$CON8)
+for (i in 10:17){
+  data[,i] = as.numeric(data[,i])
+}
+head(data[,10:17])
+
